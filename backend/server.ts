2a987d0f45e246ccc059a614472276c88142f1a2
xennia316 <xennia316@gmail.com>
2024-02-07 20:34:00 +0100

    fix: change server extension

--- backend/server.ts
+++ backend/server.ts
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-console.log(app, "app");
 
 app.use(express.json());
 app.use(cors());
+
+const connect = require("./mongodb");
+
+connect();
 
 app.listen(5000, () => {
 	console.log("Server is running on port 5000");
