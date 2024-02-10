dc7dc28f5aeee3432584310ce49f83677d6ca19b
xennia316 <xennia316@gmail.com>
2024-02-10 11:38:51 +0100

    add: full signup screen

--- frontend/Do-am/components/buttons/PrimaryButton.jsx
+++ frontend/Do-am/components/buttons/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 const PrimaryButton = ({ text }) => {
 	return (
-		<TouchableOpacity className=" flex items-center justify-center bg-primary w-11/12 h-10 rounded-xl">
+		<TouchableOpacity className=" flex items-center justify-center bg-primary w-full h-10  rounded-2xl my-2">
 			<Text className=" text-base text-white">{text}</Text>
 		</TouchableOpacity>
 	);
