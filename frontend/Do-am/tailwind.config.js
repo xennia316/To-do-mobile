fbc9d81fb99274b81b5863ba2a0115b9a7e4919d
xennia316 <xennia316@gmail.com>
2024-02-10 10:16:01 +0100

    add: tailwind css

---
+++ frontend/Do-am/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+
+module.exports = {
+	content: [
+		"./App.{js,jsx,ts,tsx}",
+		"./screens/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#9483bf",
+			},
+		},
+	},
+	plugins: [],
+};
