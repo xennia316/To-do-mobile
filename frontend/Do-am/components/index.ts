c412e17ac3a17dea1c126164551d908d1a475573
xennia316 <xennia316@gmail.com>
2024-02-12 22:17:34 +0100

    fix: mount task component

--- frontend/Do-am/components/index.ts
+++ frontend/Do-am/components/index.ts
@@ -1,5 +1,6 @@
 import PrimaryButton from "./buttons/PrimaryButton";
 import PrimaryInput from "./inputs/PrimaryInput";
 import CollapsibleView from "./collapsibleView/CollapsibleView";
+import Task from "./task/Task";
 
-export { PrimaryButton, PrimaryInput, CollapsibleView };
+export { PrimaryButton, PrimaryInput, CollapsibleView, Task };
---
+++ frontend/Do-am/components/task/Task.jsx
@@ -0,0 +1,19 @@
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import React from "react";
+import { Text, View } from "react-native";
+
+const Task = ({ iconName, task, subText, time }) => {
+	return (
+		<View className="flex-row h-16 bg-white  justify-between items-center my-2 border-2 border-slate-200 rounded-xl px-1 py-2">
+			<FontAwesomeIcon color="#9483bf" size={20} icon={iconName} />
+			<View className="w-[75%]">
+				<Text className="font-semibold text-sm">{task}</Text>
+				<Text className="text-gray-400 font-light text-xs">{subText}</Text>
+			</View>
+			<Text className="text-gray-600 text-xs text-start h-full">{time}</Text>
+		</View>
+	);
+};
+
+export default Task;
